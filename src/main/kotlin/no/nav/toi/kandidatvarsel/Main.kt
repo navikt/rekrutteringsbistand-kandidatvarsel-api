47f0e037b219836c0fe6ea55f9bdae65776dc24b
Peter Brottveit Bock <peter.brottveit.bock@nav.no>
2024-05-03 09:40:39 +0200

    Koble på kafka i prod også

--- src/main/kotlin/no/nav/toi/kandidatvarsel/Main.kt
+++ src/main/kotlin/no/nav/toi/kandidatvarsel/Main.kt
@@ -2,8 +2,6 @@ package no.nav.toi.kandidatvarsel
 
 import no.nav.toi.kandidatvarsel.minside.bestillVarsel
 import no.nav.toi.kandidatvarsel.minside.sjekkVarselOppdateringer
-import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.clients.producer.KafkaProducer
 import org.flywaydb.core.api.output.MigrateResult
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -42,41 +40,35 @@ fun main() {
     val shutdown = AtomicBoolean(false)
 
     val kafkaConfig = KafkaConfig.nais()
-    var minsideBestillingProducer: KafkaProducer<String, String>? = null
-    var minsideOppdateringConsumer: KafkaConsumer<String, String>? = null
-    var minsideBestillingThread: Thread? = null
-    var minsideOppdateringThread: Thread? = null
 
-    if (getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
-        minsideBestillingProducer = kafkaConfig.minsideBestillingsProducer()
-        minsideOppdateringConsumer = kafkaConfig.minsideOppdateringsConsumer()
+    val minsideBestillingProducer = kafkaConfig.minsideBestillingsProducer()
+    val minsideOppdateringConsumer = kafkaConfig.minsideOppdateringsConsumer()
 
-        val azureTokenClient = AzureTokenClient(
-            tokenEndpoint = getenvOrThrow("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
-            clientId = getenvOrThrow("AZURE_APP_CLIENT_ID"),
-            clientSecret = getenvOrThrow("AZURE_APP_CLIENT_SECRET"),
-            scope = "api://${getenv("NAIS_CLUSTER_NAME")}.toi.rekrutteringsbistand-stilling-api/.default"
-        )
+    val azureTokenClient = AzureTokenClient(
+        tokenEndpoint = getenvOrThrow("AZURE_OPENID_CONFIG_TOKEN_ENDPOINT"),
+        clientId = getenvOrThrow("AZURE_APP_CLIENT_ID"),
+        clientSecret = getenvOrThrow("AZURE_APP_CLIENT_SECRET"),
+        scope = "api://${getenv("NAIS_CLUSTER_NAME")}.toi.rekrutteringsbistand-stilling-api/.default"
+    )
 
-        val stillingClient = StillingClientImpl(azureTokenClient)
+    val stillingClient = StillingClientImpl(azureTokenClient)
 
-        minsideBestillingThread = backgroundThread("minside-utsending", shutdown) {
-            if (!bestillVarsel(dataSource, stillingClient, minsideBestillingProducer)) {
-                Thread.sleep(1.seconds.inWholeMilliseconds)
-            }
+    val minsideBestillingThread = backgroundThread("minside-utsending", shutdown) {
+        if (!bestillVarsel(dataSource, stillingClient, minsideBestillingProducer)) {
+            Thread.sleep(1.seconds.inWholeMilliseconds)
         }
+    }
 
-        minsideOppdateringThread = backgroundThread(name = "minside-oppdatering", shutdown) {
-            sjekkVarselOppdateringer(dataSource, minsideOppdateringConsumer)
-        }
+    val minsideOppdateringThread = backgroundThread(name = "minside-oppdatering", shutdown) {
+        sjekkVarselOppdateringer(dataSource, minsideOppdateringConsumer)
     }
 
     Runtime.getRuntime().addShutdownHook(Thread {
         shutdown.set(true)
-        minsideBestillingThread?.join()
-        minsideBestillingProducer?.close()
-        minsideOppdateringThread?.join()
-        minsideOppdateringConsumer?.close()
+        minsideBestillingThread.join()
+        minsideBestillingProducer.close()
+        minsideOppdateringThread.join()
+        minsideOppdateringConsumer.close()
         javalin.stop()
         dataSource.close()
         log.info("Shutdownhook ran successfully to completion")
