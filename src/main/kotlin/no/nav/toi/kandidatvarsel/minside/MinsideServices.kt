87ced8740861e333f42fc1fd070087d750bc55dc
Joar Aurdal <13847640+joarau@users.noreply.github.com>
2025-12-03 15:08:05 +0100

    Ta bort unødvendig logging

--- src/main/kotlin/no/nav/toi/kandidatvarsel/minside/MinsideServices.kt
+++ src/main/kotlin/no/nav/toi/kandidatvarsel/minside/MinsideServices.kt
@@ -3,7 +3,8 @@ package no.nav.toi.kandidatvarsel.minside
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
-import no.nav.toi.kandidatvarsel.*
+import no.nav.toi.kandidatvarsel.StillingClient
+import no.nav.toi.kandidatvarsel.transaction
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.producer.Producer
 import org.slf4j.LoggerFactory
@@ -20,20 +21,20 @@ fun bestillVarsel(
     stillingClient: StillingClient,
     kafkaProducer: Producer<String, String>,
 ): Boolean = dataSource.transaction { tx ->
-    val minsideVarsel = MinsideVarsel.finnOgLåsUsendtVarsel(tx) ?:
-        return@transaction false
-    
+    val minsideVarsel = MinsideVarsel.finnOgLåsUsendtVarsel(tx) ?: return@transaction false
+
     when (val mal = minsideVarsel.mal) {
         is StillingMal -> {
             val stilling = stillingClient.getStilling(UUID.fromString(minsideVarsel.avsenderReferanseId))
                 ?: throw IllegalStateException("Kunne ikke hente stilling med id ${minsideVarsel.avsenderReferanseId}")
             kafkaProducer.sendBestilling(minsideVarsel, mal, stilling.title, stilling.businessName)
         }
+
         is RekrutteringstreffMal -> {
             kafkaProducer.sendBestilling(minsideVarsel, mal)
         }
     }
-    
+
     val oppdatertVarsel = minsideVarsel.markerBestilt()
     oppdatertVarsel.save(tx)
     return@transaction true
@@ -59,22 +60,18 @@ fun sjekkVarselOppdateringer(
                 .toMutableMap()
 
             oppdateringer.forEach { oppdatering ->
-                log.info("Mottatt oppdatering for varselId ${oppdatering.varselId}, type: ${oppdatering::class.simpleName}")
-
                 val varsel = varsler[oppdatering.varselId]
-                if (varsel == null) {
-                    log.info("Fant ikke varsel med id ${oppdatering.varselId}, ignorerer oppdatering")
-                    return@forEach
-                }
+                if (varsel != null) {
 
-                val oppdatertVarsel = varsel.oppdaterFra(oppdatering)
-                oppdatertVarsel.save(tx)
-                varsler[oppdatering.varselId] = oppdatertVarsel
-                log.info("Lagret oppdatering for varsel ${oppdatering.varselId}, status: ${oppdatertVarsel.eksternStatus}, partitionOffset: ${oppdatering.partitionOffset}")
+                    val oppdatertVarsel = varsel.oppdaterFra(oppdatering)
+                    oppdatertVarsel.save(tx)
+                    varsler[oppdatering.varselId] = oppdatertVarsel
+                    log.info("Lagret oppdatering for varsel ${oppdatering.varselId}, status: ${oppdatertVarsel.eksternStatus}, partitionOffset: ${oppdatering.partitionOffset}")
 
-                if (oppdatertVarsel.mal.brukerRapid() && oppdatertVarsel.skalPubliseresPåRapid()) {
-                    log.info("Publiserer varsel ${oppdatering.varselId} på rapid, status: ${oppdatertVarsel.eksternStatus}, partitionOffset: ${oppdatering.partitionOffset}")
-                    publiserPåRapid(oppdatertVarsel, rapidsConnection)
+                    if (oppdatertVarsel.mal.brukerRapid() && oppdatertVarsel.skalPubliseresPåRapid()) {
+                        log.info("Publiserer varsel ${oppdatering.varselId} på rapid, status: ${oppdatertVarsel.eksternStatus}, partitionOffset: ${oppdatering.partitionOffset}")
+                        publiserPåRapid(oppdatertVarsel, rapidsConnection)
+                    }
                 }
             }
         }
@@ -83,22 +80,22 @@ fun sjekkVarselOppdateringer(
 
 private fun publiserPåRapid(varsel: MinsideVarsel, rapidsConnection: RapidsConnection) {
 
-        val responseDto = varsel.toResponse()
-        val opprettetZoned = responseDto.opprettet.atZone(java.time.ZoneId.of("Europe/Oslo"))
-        val packet = mapOf(
-            "@event_name" to "minsideVarselSvar",
-            "varselId" to responseDto.id,
-            "avsenderReferanseId" to responseDto.stillingId,
-            "fnr" to responseDto.mottakerFnr,
-            "eksternStatus" to responseDto.eksternStatus,
-            "minsideStatus" to responseDto.minsideStatus,
-            "opprettet" to opprettetZoned.format(java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME),
-            "avsenderNavident" to responseDto.avsenderNavident,
-            "eksternFeilmelding" to responseDto.eksternFeilmelding,
-            "eksternKanal" to responseDto.eksternKanal,
-            "mal" to varsel.mal.name
-        )
-        rapidsConnection.publish(responseDto.mottakerFnr, objectMapper.writeValueAsString(packet))
+    val responseDto = varsel.toResponse()
+    val opprettetZoned = responseDto.opprettet.atZone(java.time.ZoneId.of("Europe/Oslo"))
+    val packet = mapOf(
+        "@event_name" to "minsideVarselSvar",
+        "varselId" to responseDto.id,
+        "avsenderReferanseId" to responseDto.stillingId,
+        "fnr" to responseDto.mottakerFnr,
+        "eksternStatus" to responseDto.eksternStatus,
+        "minsideStatus" to responseDto.minsideStatus,
+        "opprettet" to opprettetZoned.format(java.time.format.DateTimeFormatter.ISO_OFFSET_DATE_TIME),
+        "avsenderNavident" to responseDto.avsenderNavident,
+        "eksternFeilmelding" to responseDto.eksternFeilmelding,
+        "eksternKanal" to responseDto.eksternKanal,
+        "mal" to varsel.mal.name
+    )
+    rapidsConnection.publish(responseDto.mottakerFnr, objectMapper.writeValueAsString(packet))
 
 }
 
